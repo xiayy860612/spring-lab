b84021efa2df18e6ff53f076e806423643f1e1b0
Yangyang Xia <Yangyang.Xia@ygomi.com>
2019-04-02 17:06:43 +0800

    update

--- spring-auth/src/main/java/com/s2u2m/lab/spring/auth/config/SecurityConfig.java
+++ spring-auth/src/main/java/com/s2u2m/lab/spring/auth/config/SecurityConfig.java
@@ -3,28 +3,14 @@ package com.s2u2m.lab.spring.auth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Override
-//    protected UserDetailsService userDetailsService() {
-//        // ensure the passwords are encoded properly
-//        User.UserBuilder users = User.withDefaultPasswordEncoder();
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        manager.createUser(users.username("admin").password("password").roles("USER","ADMIN").build());
-//        return manager;
-//    }
-//
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new PasswordEncoder() {
