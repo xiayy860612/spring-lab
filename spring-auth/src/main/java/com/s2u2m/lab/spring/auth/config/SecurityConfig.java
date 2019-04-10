430db27fd74124b0adc39ac1bf4e57713a6561c8
Yangyang Xia <Yangyang.Xia@ygomi.com>
2019-04-10 09:24:54 +0800

    update

--- spring-auth/src/main/java/com/s2u2m/lab/spring/auth/config/SecurityConfig.java
+++ spring-auth/src/main/java/com/s2u2m/lab/spring/auth/config/SecurityConfig.java
@@ -3,12 +3,14 @@ package com.s2u2m.lab.spring.auth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
